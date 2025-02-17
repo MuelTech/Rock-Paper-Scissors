1df98e831ede163add3cb5e7cc887a3a7879ac0e
John Lemuel Teano <lemuelteano@gmail.com>
2025-02-17 19:16:07 +0800

    Add getComputerChoice function

--- my-script.js
+++ my-script.js
@@ -1,1 +1,11 @@
-console.log('Hello World!');
+function getComputerChoice() {
+    
+    let comChoice = Math.floor(Math.random() * 3) + 1;
+
+    switch(comChoice) {
+        case 1: return "rock";
+        case 2: return "paper";
+        default: return "scissors";
+    }
+}
+
