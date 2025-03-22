e70fe086067a6180dc7755ce912bb404e48eca89
John Lemuel Teano <lemuelteano@gmail.com>
2025-03-22 23:31:38 +0800

    Refactor Rock Paper Scissors game logic and improve UI functionality

--- my-script.js
+++ my-script.js
@@ -1,11 +1,37 @@
-
 let humanScore = 0;
 let computerScore = 0;
+let roundGame = 0;
 
+const startButton = document.getElementById("rps-btn");
+const para = document.querySelector(".game-message");
+const humanChoice = document.querySelector('.human-choice');
+const computerChoice = document.querySelector('.computer-choice');
+const roundText = document.querySelector('.round-text');
+const humanScoreText = document.querySelector('.human-score');
+const computerScoreText = document.querySelector('.computer-score')
+const showWinner = document.querySelector('.popup-modal');
+const modalMessage = document.querySelector('.popup-message');
+const resetBtn = document.querySelector(".popup-btn");
+const disableWeaponBtn = document.querySelectorAll(".disabled-btn")
 
+startButton.addEventListener('click', (event) => {
+    playGame(event.target.value);
+});
+
+resetBtn.addEventListener('click', () => gameReset());
+
+function showRoundChoices(humanPick, computerPick) {
+    humanChoice.textContent = convertIcon(humanPick);
+    computerChoice.textContent = convertIcon(computerPick);
+}
+
+function convertIcon (convertTextToIcon) {
+    if (convertTextToIcon == 'rock') return "👊";
+    else if (convertTextToIcon == 'paper') return "🖐️";
+    else return "✌️";
+}
 
 function getComputerChoice() {
-    
     let comChoice = Math.floor(Math.random() * 3) + 1;
 
     switch(comChoice) {
@@ -22,50 +48,57 @@ function getHumanChoice(choice) {
 } 
 
 function playRound(humanChoice, computerChoice) {
-
+    
     if (humanChoice === computerChoice) {
-        para.textContent = ("ITS A TIE!\n");
-        para.textContent += (`Human Score: ${humanScore}\nComputer Score: ${computerScore}`);
+        para.textContent = "ITS A TIE!";
+        roundText.textContent = `Round ${roundGame++}`;
     } else if (humanChoice === 'rock' && computerChoice === 'scissors' ||
         humanChoice === 'paper' && computerChoice === 'rock' ||
         humanChoice === 'scissors' && computerChoice === 'paper')
     {
         humanScore++;
         para.textContent = (`You Win! ${humanChoice} beats ${computerChoice}\n`);
-        para.textContent += (`Human Score: ${humanScore}\nComputer Score: ${computerScore}`);
+        humanScoreText.textContent = humanScore;
+        computerScoreText.textContent = computerScore;
+        roundText.textContent = `Round ${roundGame++}`;
     } else {
         computerScore++;
         para.textContent = (`You Lose! ${computerChoice} beats ${humanChoice}\n`);
-        para.textContent += (`Human Score: ${humanScore}\nComputer Score: ${computerScore}`);
+        humanScoreText.textContent = humanScore;
+        computerScoreText.textContent = computerScore;
+        roundText.textContent = `Round ${roundGame++}`;
     }
 }
 
-// function playGame() {
+function showTheWinner() {
+    if (humanScore === 5) {
+        showWinner.classList.remove('show')
+        disableWeaponBtn.forEach(elem => elem.disabled = true);
+    } else if (computerScore === 5) {
+        modalMessage.textContent = "CPU WON!";
+        showWinner.classList.remove('show')
+        disableWeaponBtn.forEach(elem => elem.disabled = true);
+    }
+}
 
-//     for (let i = 1; i <= 5; i++) {
-//         const humanSelection = getHumanChoice();
-//         const computerSelection = getComputerChoice();
-//         playRound(humanSelection, computerSelection, humanScore, computerScore);
-//     } 
-//     if (humanScore === computerScore) {
-//         console.log(`It's a draw! No winner in this match.`)
-//     }else if (humanScore > computerScore) {
-//         console.log(`You beat Computer! Congratulations!`);
-//     } else {
-//         console.log(`GAME OVER! You lost by a Computer!`);
-//     }
-// }
+function gameReset() {
+        roundGame = 0;
+        humanScore = 0;
+        computerScore = 0;
+        showWinner.classList.add('show')
+        humanChoice.textContent = "?";
+        computerChoice.textContent = "?";
+        para.innerHTML = "Ready to Play? <br> Make your Move!";
+        roundText.textContent = `Round ${roundGame}`;
+        humanScoreText.textContent = humanScore;
+        computerScoreText.textContent = computerScore;
+        disableWeaponBtn.forEach(elem => elem.disabled = false);
+}
 
-    const buttons = document.getElementById("buttons");
-    const para = document.querySelector(".announce");
-    para.style.whiteSpace = "pre";
-
-    buttons.addEventListener("click", (choices) => {
-        if (choices.target.matches(".btn")) {
-            const humanSelection = getHumanChoice(choices.target.textContent);
-            const computerSelection = getComputerChoice();
-            playRound(humanSelection, computerSelection);
-        }
-    });
-
-// playGame();
+function playGame(humanSelect) {
+        const humanSelection = getHumanChoice(humanSelect);
+        const computerSelection = getComputerChoice();
+        showRoundChoices(humanSelection, computerSelection);
+        playRound(humanSelection, computerSelection);
+        showTheWinner();
+}
